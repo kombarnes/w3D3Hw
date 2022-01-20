e8eee9f0e21c28eb2e728349ef3cd0c76423afcb
Kourtney <barnes.kourt@gmail.com>
2022-01-19 21:13:44 -0500

    push

---
+++ app.js
@@ -0,0 +1,29 @@
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+var f = 6;
+var g = 7;
+var h = 8;
+var i = 9;
+var j = 10;
+
+console.log(a+b)
+console.log(c+d)
+
+console.log(j-i)
+console.log(g-c)
+
+console.log(e*f)
+console.log(j*d)
+
+console.log(j/b)
+console.log(i/c)
+
+b++;
+c++;
+
+d--;
+f--;
+
